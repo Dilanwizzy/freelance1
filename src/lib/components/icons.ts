183660b16ad5984a1a5ca11ff787ee7897fa8725
Dilanwizzy <dilan@digitalplug.co.uk>
2024-05-03 11:33:53 +0100

    Added heart share and comment icons

--- src/lib/components/icons.ts
+++ src/lib/components/icons.ts
@@ -13,8 +13,12 @@ import IHair from '~icons/material-symbols/face-4-outline';
 // import ISearch from '~icons/material-symbols/search-rounded';
 import IProfile from '~icons/material-symbols/person-rounded';
 import IBusiness from '~icons/material-symbols/business-chip-outline';
-
+import ISend from '~icons/material-symbols/send-rounded';
+import IComment from '~icons/material-symbols/mode-comment-rounded';
+import IHeart from '$lib/components/elements/heart.svelte';
 type IconComponent = { name: string; component: typeof SvelteComponent };
+
+
 
 export const IconList: IconComponent[] = [
 	{ name: 'location-dot', component: ILocation },
@@ -31,4 +35,7 @@ export const IconList: IconComponent[] = [
 	{ name: 'health', component: IHealthAndBeauty },
 	{ name: 'profile', component: IProfile },
 	{ name: 'business', component: IBusiness },
+	{ name: 'heart', component:  IHeart },
+	{ name: 'share', component: ISend },
+	{ name: 'comment', component: IComment }
 ];
